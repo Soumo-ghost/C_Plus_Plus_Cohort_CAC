ffb674896374e1fc87ed70cc284344914f5ff5fb
Soumo-ghost <chakrabortypremanshu@gmail.com>
2025-11-05 13:41:16 +0530

    Structure Of Basic Frame

--- Basic/01_hello.cpp
+++ Basic/01_hello.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
 using namespace std;
-int main(){
-    
-    
-    
+// using std::cout;
+// using std::endl;
+//----------this also used for ln12 pperpose---------
+//We can made Custom namespace
+//populer namespane Qt feramework to made wedigts and other staff
+//egien c++ are highlevel libery use for ML 
+//gtest also used for esting by google
+int main(){ 
+    //std::cout<< "hello bro"<<std::endl;
+    //above used(not writing ln2) not to load whole namespace std for optimization
+    cout <<"hi cpp is running"<<endl;
     return 0;
+    // 0 means succefully exited after running 
 }
