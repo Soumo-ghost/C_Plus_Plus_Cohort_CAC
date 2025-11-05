50e75495c333c008ddb0086229ddc215ffeceea0
Soumo-ghost <chakrabortypremanshu@gmail.com>
2025-11-05 15:19:36 +0530

    const like js

---
+++ Basic/02_varconst.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"hello"<<endl;
+    int score;
+    score=110;
+    int ballance=10000;
+    //identifier are name that are reserve for complier likw int char return;
+    const int qid=123457;
+    //const same as js
+    return 0;
+} 
