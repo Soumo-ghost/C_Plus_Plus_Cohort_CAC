754b986396971e164548555ccd61b02de36f9aaa
Soumo-ghost <chakrabortypremanshu@gmail.com>
2025-12-13 12:23:48 +0530

    Array Start

---
+++ Arrays/01_Array.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+int chaiTemp[5]={ 85, 70 ,60 ,80 ,95};
+cout<<"Chai Temperature";
+for(int i=0;i<5;i++ ){
+cout<<chaiTemp[i]<<" Degree celcius \n";
+}
+
+    return 0;
+}
